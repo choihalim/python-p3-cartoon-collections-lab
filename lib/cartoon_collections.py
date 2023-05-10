9b814d6207d406764f9be5ab306fd4afb8f6baaa
Halim Choi <hchoicareer@gmail.com>
2023-05-10 18:23:43 -0400

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for name in names:
+        print(f"{names.index(name) + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planteer_calls):
+    return [f"{call.capitalize()}!" for call in planteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food in ["cheddar", "gouda", "camembert"]:
+            return food
+    return None
